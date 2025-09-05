cae767b66945d94ec718c177961b2b879e79720e
jason <jaml08@hotmail.com>
2025-09-05 17:24:06 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/4YouPage",
+  base: "/4YouPage/",
   plugins: [react(), tailwindcss()],
 });
